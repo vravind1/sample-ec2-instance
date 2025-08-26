36cf00d3f38f76dd864bb346c80fb7da0f48d340
Vishnu Ravindra <vishnu.ravindra@hashicorp.com>
2025-08-26 11:56:37 -0400

    micro to large

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "ec2_instance" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
   name                        = "single-instance"
-  instance_type               = "t3.micro"
+  instance_type               = "t3.large"
   monitoring                  = true
   subnet_id                   = aws_subnet.main.id
   vpc_security_group_ids      = [aws_security_group.main.id]
